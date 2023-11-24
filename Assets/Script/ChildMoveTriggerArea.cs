3c66e359675e10b9a21d449818baa3a10a77a081
Erik Forssell <Erikeforssell@gmail.com>
2023-11-24 15:27:58 +0100

    Grab Child almost done

--- Assets/Script/ChildMoveTriggerArea.cs
+++ Assets/Script/ChildMoveTriggerArea.cs
@@ -1,48 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ChildMoveTriggerArea : MonoBehaviour
-{
-    public GameObject child;
-    public float moveSpeed = 5f;
-    private bool triggered = false;
-    public Rigidbody2D rb;
+//public class ChildMoveTriggerArea : MonoBehaviour
+//{
+//    public GameObject child;
+//    public float moveSpeed = 5f;
+//    private bool triggered = false;
+//    public Rigidbody2D rb;
 
-    private void Start()
-    {
-        triggered = false;
-        rb = child.GetComponent<Rigidbody2D>();
-    }
+//    private void Start()
+//    {
+//        triggered = false;
+//        rb = child.GetComponent<Rigidbody2D>();
+//    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            triggered = true;
-        }
-        else if (collision.gameObject.CompareTag("child"))
-        {
-            triggered = false;
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("Player"))
+//        {
+//            triggered = true;
+//        }
+//        else if (collision.gameObject.CompareTag("child"))
+//        {
+//            triggered = false;
 
-        }
-    }
+//        }
+//    }
 
 
-    private void Update()
-    {
-        Debug.Log(triggered);
-        if (triggered)
-        {
+//    private void Update()
+//    {
 
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+//        if (triggered && child != null)
+//        {
 
-        }
-        else if (!triggered)
-        {
+//            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
 
-            rb.velocity = new Vector2(0, rb.velocity.y);
+//        }
+//        else if (!triggered && child != null)
+//        {
 
-        }
-    }
-}
+//            rb.velocity = new Vector2(0, rb.velocity.y);
+
+//        }
+//    }
+//}
--- Assets/Script/GrabChild.cs
+++ Assets/Script/GrabChild.cs
@@ -1,35 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GrabChild : MonoBehaviour
 {
     public GrabArea grabArea;
-    public MotherMovementTemp motherMovement;
+    private MotherMovementTemp motherMovement;
     public GameObject child;
+    private Rigidbody2D childRb;
+    private Rigidbody2D motherRb;
+    private float forceDecrease = 4.5f;
+    private int repeatCount = 0; // Variable to track the number of repetitions
 
     void Start()
     {
         grabArea = GetComponentInChildren<GrabArea>();
         motherMovement = GetComponent<MotherMovementTemp>();
+        childRb = child.GetComponent<Rigidbody2D>();
+        motherRb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Debug.Log(grabArea.ChildGrab);
-
-        if (grabArea.ChildGrab == true && !motherMovement.IsGrounded())
+        if (grabArea.ChildGrab && !motherMovement.IsGrounded() && child != null)
         {
             AttachChildToMother();
+            childRb.isKinematic = true;
         }
+        else if (motherMovement.IsGrounded() && child != null)
+        {
+            DetachChildFromMother();
+            childRb.isKinematic = false;
+        }
+
+
+        
     }
 
     void AttachChildToMother()
     {
+        if (child != null)
+        {
+            child.transform.SetParent(transform);
+            MoveChildInAir();
+        }
+    }
 
-        child.transform.SetParent(transform);
+    void DetachChildFromMother()
+    {
+        if (child != null && child.transform.parent == transform)
+        {
+
+            child.transform.SetParent(null);
+
+            CancelInvoke("AddForceToChild");
+
+            InvokeRepeating("AddForceToChild", 0, 0.2f);
+        }
+    }
+
+    void AddForceToChild()
+    {
+        
+        childRb.AddForce(new Vector2(transform.localScale.x * forceDecrease, forceDecrease / 2f), ForceMode2D.Impulse);
+
+        forceDecrease -= 1.5f;
 
 
-        child.transform.localPosition = new Vector2(0.8f, 0.8f);
+        repeatCount++;
+        if (repeatCount >= 3)
+        {
+
+            CancelInvoke("AddForceToChild");
+            forceDecrease = 3;
+            repeatCount = 0;
+        }
+    }
+
+    void MoveChildInAir()
+    {
+        Vector2 startPosition = child.transform.localPosition;
+
+        child.transform.localPosition = Vector2.Lerp(startPosition, new Vector2(0.7f, 0f), Time.deltaTime*7);
+        
+
+        Debug.Log(startPosition);
     }
 }
