c2683aeea0d121423434fa6ac2073f7353f80ab6
Erik Forssell <Erikeforssell@gmail.com>
2023-11-28 09:21:00 +0100

    change state triggers

---
+++ Assets/Script/StateTriggerAreas/ChangeStateMoveLeft.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ChildAnimationEvents;
+
+public class ChangeStateMoveLeft : MonoBehaviour
+{
+    private ChildAnimationEvents childAnimationEvents;
+
+    void Start()
+    {
+
+        childAnimationEvents = FindObjectOfType<ChildAnimationEvents>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (childAnimationEvents != null && other.CompareTag("child"))
+        {
+            childAnimationEvents.ChangeState(childState.MovingLeft);
+        }
+    }
+}
