64e0e4adeb1f7bd7f90a700b657851594d5004c9
Erik Forssell <Erikeforssell@gmail.com>
2023-12-18 20:54:03 +0100

    Too far from child shader/script

---
+++ Assets/Script/LonelyScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class LonelyScript : MonoBehaviour
+{
+    private Renderer lonelyRenderer;
+    private Material uniqueMaterial;
+    public Transform child;
+    private float currentAlpha = 1.5f;
+
+    private float maxDistance = 3f;
+    private float speed = 3f;
+
+    private void Start()
+    {
+        lonelyRenderer = GetComponent<Renderer>();
+        uniqueMaterial = lonelyRenderer.material;
+    }
+
+    void Update()
+    {
+        float distance = Mathf.Abs(child.position.x - transform.position.x);
+
+        if (distance > maxDistance)
+        {
+            currentAlpha += speed * Time.deltaTime;
+        } 
+        else
+        {
+            currentAlpha -= (speed * 5) * Time.deltaTime;
+        }
+
+
+        uniqueMaterial.SetFloat("_CircleSize", currentAlpha);
+    }
+}
+
+
