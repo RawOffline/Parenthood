aa266112fb435ad24441a9afcc6dda5b7d7262ab
Erik Forssell <Erikeforssell@gmail.com>
2023-12-27 14:06:36 +0100

    Update crushingBlock.cs

--- Assets/Script/crushingBlock.cs
+++ Assets/Script/crushingBlock.cs
@@ -12,34 +12,75 @@ public class CrushingBlock : MonoBehaviour
     private bool canHurtChild = false;
     private bool hitPlayer = false;
 
-    public float upspeed;
-    public float downspeed;
-    public Transform up;
-    public Transform down;
-    bool chop;
-    // Start is called before the first frame update
-    void Start()
+    private CheckpointManager checkpointManager;    
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        checkpointManager = FindObjectOfType<CheckpointManager>();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.y > up.position.y)
-        {
-            chop = true;
-        }
-        if (transform.position.y <= down.position.y)
-        {
-            chop = false;
-        }
 
-        if (chop)
+    private void Update()
+    {
+        if (isGoingUp)
         {
-            transform.position = Vector2.MoveTowards(transform.position, down.position, downspeed * Time.deltaTime);
+            canHurtChild = false;
+            MoveUpToPointA();
         }
-        else
+        else if (!isGoingUp && !hitPlayer)
         {
-            transform.position = Vector2.MoveTowards(transform.position, up.position, upspeed * Time.deltaTime);
+            canHurtChild = true;
+            MoveUpToPointB();
+            
+        }
+        
+    }
+
+    private void MoveUpToPointA()
+    {
+        Vector2 direction = (pointA.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(rb.velocity.x, direction.y * 2);
+
+        if (transform.position.y >= pointA.transform.position.y - 0.2f)
+        {
+            rb.velocity = Vector2.zero;
+            isGoingUp = false;
+        }
+    }
+
+    private void MoveUpToPointB()
+    {
+        
+        Vector2 direction = (pointB.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(rb.velocity.x, direction.y * 100);
+        
+        if (transform.position.y <= pointB.transform.position.y)
+        {
+
+            StartCoroutine(WaitAndMoveUp());
+        }
+    }
+
+    private IEnumerator WaitAndMoveUp()
+    {
+        canHurtChild = false;
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(2);
+        hitPlayer = false;
+        isGoingUp = true;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("child") && canHurtChild)
+        {
+            checkpointManager.LoadCheckpoint();
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hitPlayer = true;
+            StartCoroutine(WaitAndMoveUp());
         }
     }
 }
